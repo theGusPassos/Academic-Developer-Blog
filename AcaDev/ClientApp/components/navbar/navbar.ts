d8a19837bcc39887f6b27bfdb3cf19bb21ad6f7f
Gustavo Passos <theguspassos@gmail.com>
2019-03-19 23:47:47 -0300

    Add mobile nav menu

---
+++ AcaDev/ClientApp/components/navbar/navbar.ts
@@ -0,0 +1,11 @@
+﻿import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class NavbarComponent extends Vue {
+    showMobileNav: boolean = false;
+
+    onHamburgerClick() {
+        this.showMobileNav = !this.showMobileNav;
+    }
+}
