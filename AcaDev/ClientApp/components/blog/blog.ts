ea82f0f181ce5efdef935a184151e717346b7971
Gustavo Passos <theguspassos@gmail.com>
2019-03-23 15:18:43 -0300

    Add comment section to post page

--- AcaDev/ClientApp/components/blog/blog.ts
+++ AcaDev/ClientApp/components/blog/blog.ts
@@ -17,10 +17,10 @@ export default class BlogComponent extends Vue {
     fetchPosts() {
         // mocked posts
         this.posts = [
-            { id: 1, title: 'Teste Teste Teste Teste Trying to Creating new Stuff', date: new Date(), author: 'Gustavo Passos', tags: ['.net core'], content: 'Ullamco enim cillum nostrud officia tempor. Nostrud do id laboris eu id fugiat aliquip laboris aute veniam sint aute. Sunt ad sint consectetur cillum duis adipisicing sit id duis.' },
-            { id: 1, title: 'Trying to Creating new Stuff II ', date: new Date(), author: 'Gustavo Passos', tags: ['.net core', 'architecture'], content: 'Ullamco enim cillum nostrud officia tempor. Nostrud do id laboris eu id fugiat aliquip laboris aute veniam sint aute. Sunt ad sint consectetur cillum duis adipisicing sit id duis.' },
-            { id: 1, title: 'Trying to Creating new Stuff III', date: new Date(), author: 'Gustavo Passos', tags: ['html', 'css'], content: 'Ullamco enim cillum nostrud officia tempor. Nostrud do id laboris eu id fugiat aliquip laboris aute veniam sint aute. Sunt ad sint consectetur cillum duis adipisicing sit id duis.' },
-            { id: 1, title: 'Trying to Creating new Stuff IV', date: new Date(), author: 'Gustavo Passos', tags: ['.net core', 'bootstrap'], content: 'Ullamco enim cillum nostrud officia tempor. Nostrud do id laboris eu id fugiat aliquip laboris aute veniam sint aute. Sunt ad sint consectetur cillum duis adipisicing sit id duis.' }
+            { id: 1, title: 'Teste Teste Teste Teste Trying to Creating new Stuff', date: new Date(), author: 'Gustavo Passos', tags: ['.net core'], content: 'Ullamco enim cillum nostrud officia tempor. Nostrud do id laboris eu id fugiat aliquip laboris aute veniam sint aute. Sunt ad sint consectetur cillum duis adipisicing sit id duis.', comments: [] },
+            { id: 1, title: 'Trying to Creating new Stuff II ', date: new Date(), author: 'Gustavo Passos', tags: ['.net core', 'architecture'], content: 'Ullamco enim cillum nostrud officia tempor. Nostrud do id laboris eu id fugiat aliquip laboris aute veniam sint aute. Sunt ad sint consectetur cillum duis adipisicing sit id duis.', comments: [] },
+            { id: 1, title: 'Trying to Creating new Stuff III', date: new Date(), author: 'Gustavo Passos', tags: ['html', 'css'], content: 'Ullamco enim cillum nostrud officia tempor. Nostrud do id laboris eu id fugiat aliquip laboris aute veniam sint aute. Sunt ad sint consectetur cillum duis adipisicing sit id duis.', comments: [] },
+            { id: 1, title: 'Trying to Creating new Stuff IV', date: new Date(), author: 'Gustavo Passos', tags: ['.net core', 'bootstrap'], content: 'Ullamco enim cillum nostrud officia tempor. Nostrud do id laboris eu id fugiat aliquip laboris aute veniam sint aute. Sunt ad sint consectetur cillum duis adipisicing sit id duis.', comments: [] }
         ];
     }
 }
