18365aeb396a88cbc29e00ea8c6f7b28fa3c2d3f
Gustavo Passos <theguspassos@gmail.com>
2019-04-02 19:08:11 -0300

    Use rest best practices for returning results

--- AcaDev/Controllers/BaseController.cs
+++ AcaDev/Controllers/BaseController.cs
@@ -1,7 +1,6 @@
 ﻿using AcaDev.Persistance.DbContexts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,12 +20,21 @@ namespace AcaDev.Controllers
 
         [HttpGet]
         [Route("{id}")]
-        public virtual Task<TEntity> Get(int id) =>
-            dbContext.Set<TEntity>().FindAsync(id);
+        public virtual async Task<IActionResult> Get(int id)
+        {
+            var entity = await dbContext.Set<TEntity>().FindAsync(id);
+            if (entity == null) return NotFound();
 
+            return Ok(entity);
+        }
 
         [HttpGet]
-        public Task<List<TEntity>> All() =>
-            dbContext.Set<TEntity>().AsQueryable().ToListAsync();
+        public async Task<IActionResult> All()
+        {
+            var entities = await dbContext.Set<TEntity>().AsQueryable().ToListAsync();
+            if (entities == null || entities.Count == 0) return NotFound();
+
+            return Ok(entities);
+        }
     }
 }
